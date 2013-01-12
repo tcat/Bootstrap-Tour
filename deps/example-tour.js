425b1bf3aa59b6e6592eb5f8e54100b4f4730457
tcat <trustut@gmail.com>
2013-01-12 12:05:33 -0800

    upd

---
+++ deps/example-tour.js
@@ -0,0 +1,57 @@
+
+jQuery(function($) {
+  var tour = new Tour();
+  tour.addStep({
+    element: "#overview",
+    placement: "bottom",
+    title: "Welcome to Bootstrap Tour!",
+    content: "Introduce new users to your product by walking them "
+    + "through it step by step. Built on the awesome "
+    + "<a href='http://twitter.github.com/bootstrap' target='_blank'>"
+    + "Bootstrap from Twitter.<\/a>"
+  });
+  tour.addStep({
+    element: "#usage",
+    placement: "right",
+    title: "Setup in four easy steps",
+    content: "Easy is better, right? Easy like Bootstrap."
+  });
+  tour.addStep({
+    path: "index.html",
+    element: "#options",
+    placement: "right",
+    title: "And it is powerful!",
+    content: "There are more options for those, like us, who want to do "
+    + "complicated things. <br \/>Power to the people! :P"
+  });
+  tour.addStep({
+    path: "page.html",
+    element: "h1",
+    placement: "bottom",
+    title: "See, you are not restricted to only one page",
+    content: "Well, nothing to see here. Click next to go back "
+    + "to the index page."
+  });
+  tour.addStep({
+    path: "index.html",
+    element: "#contributing",
+    placement: "right",
+    title: "Best of all, it's free!",
+    content: "Yeah! Free as in beer... or speech. Use and abuse, "
+    + "but don't forget to contribute!"
+  });
+  tour.start();
+
+  if ( tour.ended() ) {
+    $('<div class="alert">\
+      <button class="close" data-dismiss="alert">×</button>\
+      You ended the demo tour. <a href="" class="restart">Restart the demo tour.</a>\
+      </div>').prependTo(".content").alert();
+  }
+
+  $(".restart").click(function (e) {
+    e.preventDefault();
+    tour.restart();
+    $(this).parents(".alert").alert("close");
+  });
+});
